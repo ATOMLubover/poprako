29f1b26067bbb78ccac83e2d1bc2425e732b0986
ATOMLubover <13809006129@163.com>
2026-05-06 22:24:06 +0800

    fix: fix unindepoment index creation

--- migrations/20260425163027_create-member-table.up.sql
+++ migrations/20260425163027_create-member-table.up.sql
@@ -17,35 +17,35 @@ CREATE TABLE IF NOT EXISTS "t_member" (
     "updated_at" TIMESTAMPTZ DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX "uidx_member_user_team"
+CREATE UNIQUE INDEX IF NOT EXISTS "uidx_member_user_team"
     ON "t_member" ("user_id", "team_id");
-CREATE INDEX "idx_member_user_id"
+CREATE INDEX IF NOT EXISTS "idx_member_user_id"
     ON "t_member" ("user_id");
-CREATE INDEX "idx_member_team_id"
+CREATE INDEX IF NOT EXISTS "idx_member_team_id"
     ON "t_member" ("team_id");
 
-CREATE INDEX "idx_member_team_raw_provider"
+CREATE INDEX IF NOT EXISTS "idx_member_team_raw_provider"
     ON "t_member" ("team_id", "assigned_raw_provider_at")
     WHERE "assigned_raw_provider_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_translator"
+CREATE INDEX IF NOT EXISTS "idx_member_team_translator"
     ON "t_member" ("team_id", "assigned_translator_at")
     WHERE "assigned_translator_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_proofreader"
+CREATE INDEX IF NOT EXISTS "idx_member_team_proofreader"
     ON "t_member" ("team_id", "assigned_proofreader_at")
     WHERE "assigned_proofreader_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_typesetter"
+CREATE INDEX IF NOT EXISTS "idx_member_team_typesetter"
     ON "t_member" ("team_id", "assigned_typesetter_at")
     WHERE "assigned_typesetter_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_redrawer"
+CREATE INDEX IF NOT EXISTS "idx_member_team_redrawer"
     ON "t_member" ("team_id", "assigned_redrawer_at")
     WHERE "assigned_redrawer_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_reviewer"
+CREATE INDEX IF NOT EXISTS "idx_member_team_reviewer"
     ON "t_member" ("team_id", "assigned_reviewer_at")
     WHERE "assigned_reviewer_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_publisher"
+CREATE INDEX IF NOT EXISTS "idx_member_team_publisher"
     ON "t_member" ("team_id", "assigned_publisher_at")
     WHERE "assigned_publisher_at" IS NOT NULL;
-CREATE INDEX "idx_member_team_admin"
+CREATE INDEX IF NOT EXISTS "idx_member_team_admin"
     ON "t_member" ("team_id", "assigned_admin_at")
     WHERE "assigned_admin_at" IS NOT NULL;
 
